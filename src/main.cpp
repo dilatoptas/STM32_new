eb6b69d502f6b1d93e3e16a9f65c0d41401daa9e
suleclkky <sulecelikkaya@gmail.com>
2025-12-27 13:27:08 +0300

    Update main.cpp
    bulunsun diye

--- src/main.cpp
+++ src/main.cpp
@@ -43,3 +43,47 @@ void loop() {
 
     delay(100); // 10 Hz döngü hızı
 }
+
+/* PB8 ve PB9 Pinleri İçin I2C Test Kodu 
+#include <Arduino.h>
+#include <Wire.h>
+
+// I2C pinlerini tanımlıyoruz
+#define I2C_SDA PB9
+#define I2C_SCL PB8
+
+void setup() {
+    Serial.begin(115200);
+    delay(2000); // Terminalin açılması için kısa bir bekleme
+
+    // Standart Wire.begin() yerine pin belirterek başlatıyoruz
+    Wire.begin(I2C_SDA, I2C_SCL); 
+
+    Serial.println("\n--- I2C Tarayici Baslatiliyor (PB8/PB9) ---");
+}
+
+void loop() {
+    byte error, address;
+    int nDevices = 0;
+
+    for(address = 1; address < 127; address++) {
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+
+        if (error == 0) {
+            Serial.print("Cihaz bulundu! Adres: 0x");
+            if (address < 16) Serial.print("0");
+            Serial.println(address, HEX);
+            nDevices++;
+        }
+    }
+
+    if (nDevices == 0) {
+        Serial.println("Hicbir I2C cihazi bulunamadi. Pinleri ve baglantiyi kontrol edin.");
+    } else {
+        Serial.println("Tarama tamamlandi.\n");
+    }
+
+    delay(2000); 
+}
+*/
